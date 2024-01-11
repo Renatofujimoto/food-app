1a5bbbb87a11b4992024d3a91b2de2ad32a56c9c
Renatofujimoto <renatofujimoto2@gmail.com>
2024-01-11 15:54:18 -0300

    feat: adding promo session

---
+++ app/components/Home/PromoCard.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  promo: PromoTypes;
+};
+
+function PromoCard({ promo }: Props) {
+  return (
+    <article className="flex w-96 rounded-lg shrink-0 shadow-lg text-gray-500 hover:bg-green-200 hover:text-green-600 transition-all duration-200 ease-out">
+      <div className="flex flex-1 relative overflow-hidden">
+        <span className="absolute rotate-[50deg] top-4 right-1 px-4 py-2 bg-red-500 text-white text-center">
+          {promo.PercentOff}%OFF
+        </span>
+        <Image src={promo.img} width={200} height={200} alt={promo.title} />
+      </div>
+
+      <div className="flex flex-col flex-1 p-5 space-y-3">
+        <p className=" font-semi-bold">{promo.title}</p>
+        <p className="text-xs inline-flex items-center">
+          {" "}
+          {promo.salesQ} sells
+          <span className="h-2 w-2 bg-gray-400 rounded-full mx-2"></span>
+          {promo.likesN} Likes
+        </p>
+        <p className="text-red-500">$ {promo.price}</p>
+        <button className="form-button">Order Now</button>
+      </div>
+    </article>
+  );
+}
+
+export default PromoCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Header from "./components/Common/Header";
 import SideBar from "./components/Common/SideBar";
 import HeroSection from "./components/Home/HeroSection";
+import Promos from "./components/Home/Promos";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-white">
       <Header />
       <SideBar />
       <HeroSection />
+      <Promos />
     </div>
   );
 }
