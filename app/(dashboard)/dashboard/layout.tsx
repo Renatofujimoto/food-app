3fe9dbe9feeb51d344c98bc9a96c13d0ed25cf67
Renatofujimoto <renatofujimoto2@gmail.com>
2024-01-24 20:35:16 -0300

    feat: adding dashboard page

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,9 @@
+import DashWrapper from "./Components/DashWrapper";
+
+type DashLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default async function DashboardLayout({ children }: DashLayoutProps) {
+  return <DashWrapper>{children}</DashWrapper>;
+}
